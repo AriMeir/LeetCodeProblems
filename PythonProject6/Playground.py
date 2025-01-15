712b99f8049776db09f128d9363dc74fd7b81576
AriMeir <AriMeir95@gmail.com>
2025-01-15 23:26:09 +0200

    problem 11

--- PythonProject6/Playground.py
+++ PythonProject6/Playground.py
@@ -139,4 +139,30 @@ def canJump(self, nums):
     return False
 
 arr = [1,2,3,4,5]
-print(len(arr))
+#print(len(arr))
+
+#There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+#You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+#Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique.
+
+
+def canCompleteCircuit(self, gas, cost):
+    """
+    :type gas: List[int]
+    :type cost: List[int]
+    :rtype: int
+    """
+    total_diff = 0
+    start = 0
+    tank = 0
+
+    for i in range(0, len(gas)):
+        diff = gas[i] - cost[i]
+        total_diff += diff
+        tank += diff
+
+        if tank < 0:
+            start = i + 1
+            tank = 0
+
+    return start if total_diff >= 0 else -1
