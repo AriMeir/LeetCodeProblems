631d68dd482c4b8d2d5153a3ca74db87579d1938
AriMeir <AriMeir95@gmail.com>
2025-01-19 15:49:12 +0200

    palindrom

--- PythonProject6/Playground.py
+++ PythonProject6/Playground.py
@@ -166,3 +166,22 @@ def canCompleteCircuit(self, gas, cost):
             tank = 0
 
     return start if total_diff >= 0 else -1
+
+def isPalindrome(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    right = len(s) - 1
+    left = 0
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right +=1
+
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right += 1
+    return True
